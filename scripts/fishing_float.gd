c30b7948fc41872ea904e0cfe79104654a06339f
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-03-25 18:55:05 +0200

    Implemented the fishing rod

--- scripts/fishing_float.gd
+++ scripts/fishing_float.gd
@@ -1,15 +1,11 @@
 extends RigidBody3D
 
-@export var float_force := 1
-@onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-const water_height := -2.5
-var time_since_last_push := 0.0  # Timer to track time since last push
-var push_interval := 20.0  # Interval in seconds for the strong push
-var strong_push_force := 10.0  # Adjust the strength of the strong push here
+# Whether the float is connected to the fishing rod
+var connected: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	time_since_last_push = 0.0
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,14 +13,12 @@ func _process(delta):
 
 # Called every physics frame
 func _physics_process(delta):
-	# Update the timer
-	time_since_last_push += delta
+	pass
 
-	# Check if 20 seconds have passed
-	if time_since_last_push >= push_interval:
-		apply_central_impulse(Vector3.DOWN * strong_push_force)  # Apply a strong push upwards
-		time_since_last_push = 0  # Reset the timer
-
-	# Your existing floating logic
-	if global_position.y < water_height:
-		apply_force(Vector3.UP * float_force * gravity * 1.5)
+# Called during physics processing, allowing safe reading and modification of the object's simulation state 
+func _integrate_forces(state):
+	pass
+	
+# Handles the interaction signal from the fishing rod
+func _on_interaction():
+	pass
