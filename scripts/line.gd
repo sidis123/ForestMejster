af146cf37b092312727d6e321eeff204f2a6cb20
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-03-26 19:50:38 +0200

    Added a basic 3D line scene

---
+++ scripts/line.gd
@@ -0,0 +1,17 @@
+@tool
+class_name Line
+extends Node3D
+
+@export var line_radius: float = 0.1
+@export var line_resolution = 180
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var circle = PackedVector2Array()
+	for degree in line_resolution:
+		var x = line_radius * sin(PI * 2 * degree / line_resolution)
+		var y = line_radius * cos(PI * 2 * degree / line_resolution)
+		var coords = Vector2(x, y)
+		circle.append(coords)
+		$CSGPolygon3D.polygon = circle
