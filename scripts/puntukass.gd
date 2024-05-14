ebe42f3a0cf3826da9e24cdd72074c9f29ed560e
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2024-05-14 18:21:38 +0300

    kipsas turns into rock, puntukas eyes lit up in order to turn kipsai into rocks

--- scripts/puntukass.gd
+++ scripts/puntukass.gd
@@ -4,9 +4,21 @@ signal kipsas_toggle_movement
 
 var time_accumulator = 0.0
 var interval = 5.0
+var degam=false
+
 
 func _process(delta):
 	time_accumulator += delta
 	if time_accumulator >= interval:
 		emit_signal("kipsas_toggle_movement")
+		degam = !degam
 		time_accumulator = 0.0
+	
+	var eye1 = $Fire/GPUParticles3D
+	var eye2 = $Fire2/GPUParticles3D
+	if degam:
+		eye1.emitting = true
+		eye2.emitting = true
+	else:
+		eye1.emitting = false
+		eye2.emitting = false
