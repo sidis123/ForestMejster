6369f76bf1f1cf0a47b0d460431215094657763c
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-05-17 23:04:32 +0300

    Added glowing runes to bucket

--- scripts/fishing/bucket.gd
+++ scripts/fishing/bucket.gd
@@ -1,6 +1,7 @@
 class_name Bucket
 extends PickableDispenser
 
+@onready var mesh_material : StandardMaterial3D = get_node("../Bucket").get_active_material(0)
 @onready var pour_particles : GPUParticles3D = get_node("../WaterPourParticles")
 @onready var splash_particles : GPUParticles3D = get_node("../WaterSplashParticles")
 
@@ -19,6 +20,7 @@ func _process(_delta):
 		if _fish.is_cooked():
 			_fish.apply_impulse(global_transform.basis.y * 0.1)
 		else:
+			_enable_glow()
 			_fish_queue.append(_fish.type)
 			_fish.queue_free()
 			splash_particles.emitting = true
@@ -31,6 +33,7 @@ func can_pick_up(by: Node3D) -> bool:
 
 ## Instantiate the scene with the last fish type
 func _get_dispensable_instance():
+	_enable_glow()
 	splash_particles.emitting = true
 	var fish_type = _fish_queue.pop_back()
 	# NOTE: very manual matching of type to index in the array of dispensed scenes
@@ -44,6 +47,14 @@ func _get_dispensable_instance():
 		_:
 			return null
 
+func _enable_glow():
+	var tween = create_tween()
+	tween.tween_property(mesh_material, "emission_energy_multiplier", 8, 0.5)
+	tween.tween_callback(_disable_glow)
+
+func _disable_glow():
+	var tween = create_tween()
+	tween.tween_property(mesh_material, "emission_energy_multiplier", 0, 0.5)
 
 func _is_upside_down():
 	var up_vector = global_transform.basis.y
