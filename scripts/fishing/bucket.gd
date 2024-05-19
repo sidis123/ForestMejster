646c34b2a618ce9177b3f01a710564abba7a90a5
Lukas Vasiliauskas <lukas.vasi@gmail.com>
2024-05-19 17:52:07 +0300

    Fixed bucket collision bugs, added more grab points

--- scripts/fishing/bucket.gd
+++ scripts/fishing/bucket.gd
@@ -1,74 +1,23 @@
 class_name Bucket
-extends PickableDispenser
+extends RigidBody3D
 
-@onready var mesh_material : StandardMaterial3D = get_node("../Bucket").get_active_material(0)
-@onready var pour_particles : GPUParticles3D = get_node("../WaterPourParticles")
-@onready var splash_particles : GPUParticles3D = get_node("../WaterSplashParticles")
+@onready var handle : XRToolsPickable = get_node("../Handle")
 
-## The queue containing the types of fish in the order they've been added
-var _fish_queue : Array[Fish.FishType] = []
-
-var _fish : Fish = null
-
-func _process(_delta):
-	if _is_upside_down():
-		pour_particles.emitting = true
-	else:
-		pour_particles.emitting = false
-		
-	if _fish and not _fish.is_picked_up():
-		if _fish.is_cooked():
-			_fish.apply_impulse(global_transform.basis.y * 0.1)
-		else:
-			_enable_glow()
-			_fish_queue.append(_fish.type)
-			_fish.queue_free()
-			splash_particles.emitting = true
+# Remember some state so we can return to it when the user drops the object
+@onready var original_collision_mask : int = collision_mask
+@onready var original_collision_layer : int = collision_layer
 
 
-## Test if this object can dispense a pickup
-func can_pick_up(by: Node3D) -> bool:
-	return _fish_queue.size() > 0
+func _ready():
+	handle.picked_up.connect(_on_picked_up)
+	handle.dropped.connect(_on_dropped)
 
 
-## Instantiate the scene with the last fish type
-func _get_dispensable_instance():
-	_enable_glow()
-	splash_particles.emitting = true
-	var fish_type = _fish_queue.pop_back()
-	# NOTE: very manual matching of type to index in the array of dispensed scenes
-	match fish_type:
-		Fish.FishType.Kuoja:
-			return dispensed_scenes[0].instantiate()
-		Fish.FishType.Lynas:
-			return dispensed_scenes[1].instantiate()
-		Fish.FishType.Raude:
-			return dispensed_scenes[2].instantiate()
-		_:
-			return null
-
-func _enable_glow():
-	var tween = create_tween()
-	tween.tween_property(mesh_material, "emission_energy_multiplier", 8, 0.5)
-	tween.tween_callback(_disable_glow)
-
-func _disable_glow():
-	var tween = create_tween()
-	tween.tween_property(mesh_material, "emission_energy_multiplier", 0, 0.5)
-
-func _is_upside_down():
-	var up_vector = global_transform.basis.y
-	var dot_product = up_vector.dot(Vector3.UP)
-	return dot_product < 0
+func _on_picked_up(by: Node3D):
+	collision_layer = 0b0000_0000_0000_0001_0000_0000_0000_0000 #held object
+	collision_mask = 0 # collisions disabled
 
 
-## Save a ref to a fish that enters the bucket
-func _on_body_entered(body):
-	if body is Fish:
-		_fish = body as Fish
-
-
-## Remove the ref
-func _on_body_exited(body):
-	if body == _fish:
-		_fish = null
+func _on_dropped(by: Node3D):
+	collision_layer = original_collision_layer
+	collision_mask = original_collision_mask
