21543ab6df00becede831ad10c759907e4a2a0f9
Tomas Sakalauskas <tomass.sakalauskas@gmail.com>
2024-05-29 15:27:40 +0300

    prepared paths for 4 kipsai and 2 bezdukai, awaiting for models

--- scripts/temp_bezdukas_walking.gd
+++ scripts/temp_bezdukas_walking.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var move_speed: float = 4.0
+var animation_player: AnimationPlayer
 var is_moving = true
 var kipsas_collision = null
 var tent_instance = null
@@ -8,10 +9,16 @@ var initial_position: Vector3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-
+	animation_player = $AnimationPlayer
+	
+	if animation_player == null:
+		print("Error: AnimationPlayer not found")
+	else:
+		print("AnimationPlayer successfully loaded")
+	
 	var puntukas = get_node("/root/Staging/Scene/Main/Combat/Puntukass")
 	puntukas.kipsas_toggle_movement.connect(_on_Bezdukas_toggle_movement)
-	kipsas_collision = get_node("Cube/BezdukasDissapearingBody/CollisionShape3D")
+	kipsas_collision = get_node("BezdukasArmature/Skeleton3D/Bezdukas7Rigged/BezdukasNotDissapearingBody/CollisionShape3D")
 
 # Called when the movement state is toggled.
 func _on_Bezdukas_toggle_movement():
@@ -28,22 +35,22 @@ func _on_Bezdukas_toggle_movement():
 			initial_position = self.global_transform.origin
 			# Remove the current Kipsas node from the scene
 			self.visible = false
-			if is_instance_valid(kipsas_collision):
-				kipsas_collision.disabled = true
+			kipsas_collision.disabled = true
 		# Stop animation when not moving
-
+		#animation_player.play("[stop]")
 	else:
 		# Remove the Tent node from the scene
-		if is_instance_valid(kipsas_collision):
-			if tent_instance:
-				tent_instance.queue_free()
+		if tent_instance:
+			tent_instance.queue_free()
 		# Restore position
-			self.global_transform.origin = initial_position
-			self.visible = true
-			if is_instance_valid(kipsas_collision):
-				kipsas_collision.disabled = false
+		self.global_transform.origin = initial_position
+		self.visible = true
+		kipsas_collision.disabled = false
 		# Play walking animation when moving
-
+		animation_player.play("Walking")
 
 func _physics_process(delta: float) -> void:
-	pass
+	if is_moving and animation_player.current_animation != "Walking":
+		animation_player.play("Walking")
+	#elif not is_moving and animation_player.current_animation != "[stop]":
+		#animation_player.play("[stop]")
